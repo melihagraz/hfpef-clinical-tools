0449553df8f49022f38ad96cc316ac29153e39bd
melih agraz <37498443+melihagraz@users.noreply.github.com>
2025-08-03 19:18:18 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HFpEFClinicalDecisionTools from './components/HFpEFClinicalDecisionTools';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HFpEFClinicalDecisionTools />
+    </div>
+  );
+}
+
+export default App;
